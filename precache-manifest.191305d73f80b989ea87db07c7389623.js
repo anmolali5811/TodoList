f5bbf1b09843a75469f684e9bca4807004353dcf
Mohd Aqib <anmolali5811@gmail.com>
2021-12-15 16:20:44 +0530

    Updates

---
+++ precache-manifest.191305d73f80b989ea87db07c7389623.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c084c33f9b6be521f863ae942173021",
+    "url": "/TodoList/index.html"
+  },
+  {
+    "revision": "f38419285d605f1d2682",
+    "url": "/TodoList/static/css/main.8e767b19.chunk.css"
+  },
+  {
+    "revision": "bb12980b1bb921747a43",
+    "url": "/TodoList/static/js/2.6011a8c7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TodoList/static/js/2.6011a8c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f38419285d605f1d2682",
+    "url": "/TodoList/static/js/main.a9cfdf8f.chunk.js"
+  },
+  {
+    "revision": "141e57522120cb2dabad",
+    "url": "/TodoList/static/js/runtime-main.d512a30b.js"
+  }
+]);
